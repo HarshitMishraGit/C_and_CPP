6e427d7924ef116fe841c1402dc0343a47b174e9
Harshit Mishra <harshitlove28@outlook.com>
2022-03-04 23:36:16 +0530

    Basic Syntax of CPP

--- CPP/BasicCPP/tut1.cpp
+++ CPP/BasicCPP/tut1.cpp
@@ -1,11 +1,11 @@
 // basic syntax of cpp
-#include<iostream>
-
-using namespace std;
+#include <iostream> // this is the header file included
+// iostream means the standard i/p and o/p 
+using namespace std; // this is used to use same name fxn in diffrent header files
 
 int main (){
-    cout << "Hello World\n";
+    cout << "Hello World\n";// this is to print the o/p
     int sum = 9;// sum is a variable with int datatype
-    cout << "The sum is "<< sum;
+    cout << "The sum is "<< sum;// this is how we can print a variable with a string with simply add <<
     return 0;  
 }
